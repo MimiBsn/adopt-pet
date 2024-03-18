fc0758e0199154d861acb6458e0a6c2df3586eb7
shaza kashlan <eng.shazakashlan@gmail.com>
2024-03-18 12:01:24 +0100

    style footer

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <>
       <NavBar />
+
       <Routes>
         <Route
           path="/"
@@ -29,6 +30,7 @@ function App() {
         />
         <Route path="/rehome" element={<AddPetForm />}></Route>
       </Routes>
+
       <Footer />
     </>
   );
